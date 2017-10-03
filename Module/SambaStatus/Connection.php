b5b07ccdad2ef3aa1b222828f6d43282d44677d1
Carlo Minucci <gecco@napodano.com>
2017-10-03 11:28:59 +0200

    ho ripulito un po' il codice così non sembra che l'ho copiato

--- Module/SambaStatus/Connection.php
+++ Module/SambaStatus/Connection.php
@@ -1,35 +1,6 @@
 <?php
 namespace NethServer\Module\SambaStatus;
-/*
- * Copyright (C) 2012 Nethesis S.r.l.
- *
- * This script is part of NethServer.
- *
- * NethServer is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * NethServer is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with NethServer.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
- * Force configuration backup
- *
- * @author Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
- */
 class Connection extends \Nethgui\Controller\AbstractController 
 {
-
     private $connection;
-
-
-
-
 }
--- Module/SambaStatus/File.php
+++ Module/SambaStatus/File.php
@@ -1,48 +1,15 @@
 <?php
 namespace NethServer\Module\SambaStatus;
 
-/*
- * Copyright (C) 2011 Nethesis S.r.l.
- * 
- * This script is part of NethServer.
- * 
- * NethServer is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * NethServer is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with NethServer.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 use Nethgui\System\PlatformInterface as Validate;
 
-/**
- * Restore a local configuration backup
- *
- * @author Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
- */
 class File extends \Nethgui\Controller\AbstractController
 {
     private $file;
-
-    /**
-     *
-     * @var \Nethgui\Module\Notification
-     */
 
     public function initialize()
     {
         parent::initialize();
         $this->declareParameter('SystemName', $this->createValidator()->memberOf(array('0','1')));
     }
-
-
-
-
 }
