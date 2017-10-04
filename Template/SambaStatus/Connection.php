e526a5defade06f45258393d6c51e8a5116934c7
Carlo Minucci <gecco@napodano.com>
2017-10-04 11:07:12 +0200

    fixato il probema con smbstatus sulla versione 7.0

--- Template/SambaStatus/Connection.php
+++ Template/SambaStatus/Connection.php
@@ -1,13 +1,21 @@
 <?php
 echo $view->header()->setAttribute('template', $T('ActiveConnections'));
+
+$ver = shell_exec("cat /etc/nethserver-release | awk '{print $3}'");
+$ver = explode(".", $ver);
+if($ver[0] > 6){ $sudo = "sudo ";}
+/* thanks to @mrmarkuz, @Ctek and @netbix */
+
 $command = "getent group domadmins | cut -d \":\" -f4";
 /* thanks to @davidep */
+
 $admin = shell_exec($command);
 $admin_users = explode(",", $admin);
 echo "<div class=\"DataTable \" >";
 echo "<table><thead>\n";
 echo "<tr><th>" . $T('Username') . "</th><th>" . $T('Hostname') . "</th><th>" . $T('IPAddress') . "</th></tr><thead><tbody>\n";
-$command = "smbstatus -b | sed -e '1,4d' | awk '{print $2 \"\t\" $4 \"\t\" $5}' | sort -h";
+
+$command = $sudo . "smbstatus -b | sed -e '1,4d' | awk '{print $2 \"\t\" $4 \"\t\" $5}' | sort -h";
 $admin_shell = shell_exec($command);
 $admins = explode("\n", chop($admin_shell));
 foreach($admins as $admin){
--- Template/SambaStatus/File.php
+++ Template/SambaStatus/File.php
@@ -1,10 +1,15 @@
 <?php
 echo $view->header()->setAttribute('template', $T('LockedFiles'));
 
+$ver = shell_exec("cat /etc/nethserver-release | awk '{print $3}'");
+$ver = explode(".", $ver);
+if($ver[0] > 6){ $sudo = "sudo ";}
+/* thanks to @mrmarkuz, @Ctek and @netbix */
+
 echo "<div class=\"DataTable \" >";
 echo "<table><thead>";
 echo "<tr><th>" . $T('Share') . "</th><th>" . $T('FileName') . "</th><th>" . $T('Date') . "</th><thead><tbody>\n";
-$command= "smbstatus -L | sed -e '1,3d' | awk -F'/var/lib/nethserver' '{print $2}'";
+$command= $sudo . "smbstatus -L | sed -e '1,3d' | awk -F'/var/lib/nethserver' '{print $2}'";
 $locked = shell_exec($command);
 $locked_files = explode("\n", chop($locked));
 foreach($locked_files as $file){
