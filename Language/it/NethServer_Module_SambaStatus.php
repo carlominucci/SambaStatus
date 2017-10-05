754b000c04b9c7b6342bd9ebd47ba5cc9a9bbac8
Carlo Minucci <gecco@napodano.com>
2017-10-05 12:20:00 +0200

    perfezionata un pelo la traduzione italiana

--- Language/it/NethServer_Module_SambaStatus.php
+++ Language/it/NethServer_Module_SambaStatus.php
@@ -1,13 +1,13 @@
 <?php
 $L['SambaStatus_Title'] = "SambaStatus";
 $L['Connection_Title'] = "Connessioni attive";
-$L['File_Title'] = "File Lockati";
-$L['Joined_Title'] = "Macchine aggiunte al dominio";
+$L['File_Title'] = "File in uso";
+$L['Joined_Title'] = "Macchine a dominio";
 $L['ActiveConnections'] = "Connessioni attive";
 $L['Username'] = "Nome Utente";
 $L['Hostname'] = "Nome Host";
-$L['LockedFiles'] = "File Lockati";
+$L['LockedFiles'] = "File in uso";
 $L['IPAddress'] = "Indirizzo IP";
 $L['Share'] = "Condivisioni";
-$L['FileName'] = "NomeFile";
+$L['FileName'] = "Nome File";
 $L['Date'] = "Data";
