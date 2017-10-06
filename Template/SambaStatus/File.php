3bb7c57c0b1e058f3cc928dbbc863d75b83b8d4a
Carlo Minucci <gecco@napodano.com>
2017-10-06 12:09:02 +0200

    c'era un problema con la tabella

--- Template/SambaStatus/File.php
+++ Template/SambaStatus/File.php
@@ -8,13 +8,13 @@ if($ver[0] > 6){ $sudo = "sudo ";}
 
 echo "<div class=\"DataTable \" >";
 echo "<table>";
-echo "<tr><td><b>" . $T('Share') . "</b></td><td><b>" . $T('FileName') . "</b></td><tb><b>" . $T('Date') . "</b></td></tr>\n<tbody>\n";
+echo "<tr><td><b>" . $T('Share') . "</b></td><td><b>" . $T('FileName') . "</b></td><td><b>" . $T('Date') . "</b></td></tr>\n<tbody>\n";
 $command= $sudo . "smbstatus -L | sed -e '1,3d' | awk -F'/var/lib/nethserver' '{print $2}'";
 $locked = shell_exec($command);
 $locked_files = explode("\n", chop($locked));
 foreach($locked_files as $file){
 	$file_tmp = explode("   ", $file);
-	echo "<tr><td>" . $file_tmp[0] . "</td><td>" . $file_tmp[1] . "</td><td>" . $file_tmp[2] . "</td><tr>";
+	echo "<tr><td>" . $file_tmp[0] . "</td><td>" . $file_tmp[1] . "</td><td>" . $file_tmp[2] . "</td></tr>";
 }
 echo "<tbody></table>\n";
 echo "</div>\n";
