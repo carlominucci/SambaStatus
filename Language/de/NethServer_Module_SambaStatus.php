fd3006cd4c6d397a979a4d2da236b56fdc47c21a
Carlo Minucci <gecco@napodano.com>
2018-10-05 07:45:40 +0200

    aggiunta la lingua tedesca, grazie a @flatspin

---
+++ Language/de/NethServer_Module_SambaStatus.php
@@ -0,0 +1,14 @@
+<?php
+$L['SambaStatus_Title'] = "SambaStatus";
+$L['Connection_Title'] = "Aktive Verbindungen";
+$L['File_Title'] = "Gesperrte Dateien";
+$L['Joined_Title'] = "Computer angemeldet an Domäne";
+$L['Share_Title'] = "Freigaben";
+$L['ActiveConnections'] = "Aktive Verbindungen";
+$L['Username'] = "Username";
+$L['Hostname'] = "Hostname";
+$L['LockedFiles'] = "Gesperrte Datei";
+$L['IPAddress'] = "IP-Addresse";
+$L['Share'] = "Freigabe";
+$L['FileName'] = "Dateiname";
+$L['Date'] = "Datum";
