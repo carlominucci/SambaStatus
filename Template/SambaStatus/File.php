617ba2a2c88d300de97b08fcaf67729aaf3093f4
Carlo Minucci <gecco@napodano.com>
2018-10-30 12:54:31 +0100

    ho fatto casino nel commit precedente

--- Template/SambaStatus/File.php
+++ Template/SambaStatus/File.php
@@ -8,14 +8,19 @@ if($ver[0] > 6){ $sudo = "sudo ";}
 
 echo "<div class=\"DataTable \" >";
 echo "<table><thead>";
-echo "<tr><td><b>" . $T('Username') . "</b></td><td><b>" . $T('IPAddress') . "</td><td><b>" . $T('Date') . " - " . $T('Share') . " - " . $T('LockedFiles') . "</b></td><thead><tbody>\n";
+echo "<tr><td><b>" . $T('Username') . "</b></td><td><b>" . $T('IPAddress') . " - " . $T('Hostname') . "</td><td><b>" . $T('Date') . " - " . $T('Share') . " - " . $T('LockedFiles') . "</b></td><thead><tbody>\n";
 //thanks to @mrmarkuz
 $command = $sudo . "smbstatus -b | grep \"@\" | awk '{print $2 \"\t\" $5 \"\t\" $1}' | sort -h | uniq";
 $listusers = shell_exec($command);
 $listusers_array = explode("\n", chop($listusers));
 foreach($listusers_array as $users){
 	$users_tmp = explode("\t", $users);
-	echo "<tr><td>" . $users_tmp[0] . "</td><td>" . $users_tmp[1] . "</td><td>\n";
+	echo "<tr><td>" . $users_tmp[0] . "</td><td>";
+	$command="timeout 0.1 nmblookup -A " . $users_tmp[1] . " | grep \"<00>\" | head -1 | awk {'print $1'}";
+	$hostname = shell_exec($command);
+	echo $users_tmp[1];
+	if(isset($hostname)){ echo " - " . $hostname; }
+	echo "</td><td>\n";
 
 	// thanks to @MrE for idea
 	$command = $sudo . "smbstatus -L | grep " . $users_tmp[2] . " | awk -F \"/var/lib/nethserver/\" {'print $2'} | sed 's/   /|/g' | awk -F \"|\" {'print $3 \" - \" $1 \"/\" $2'}";	
