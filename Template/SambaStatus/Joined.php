11d3930d219745a94b9a63fe2c0d0a740e36d638
Carlo Minucci <gecco@napodano.com>
2018-12-13 14:05:49 +0100

    aumentata la verbosità dell'output. grazie a trotto... ma per cosa?

--- Template/SambaStatus/Joined.php
+++ Template/SambaStatus/Joined.php
@@ -2,9 +2,13 @@
 echo $view->header()->setAttribute('template', $T('Joined_Title'));
 
 //thanks to @mrmarkuz
-$command = "sudo /usr/bin/net ads search -P objectClass=Computer | grep 'cn: ' | cut -f 2 -d ' ' | sort";
+//$command = "sudo /usr/bin/net ads search -P objectClass=Computer | grep 'cn: ' | cut -f 2 -d ' ' | sort";
+$command = "sudo /usr/bin/net ads search -P objectClass=Computer cn operatingSystem | grep \":\" ";
 $pc = shell_exec($command);
-$arrpc = explode("\n", $pc);
+
+$pc = str_ireplace("cn: ", "|", $pc);
+$pc = str_ireplace("operatingSystem: ", " - ", $pc);
+$arrpc = explode("|", $pc);
 echo "<table>";
 echo "<tr>";
 echo "<td>";
@@ -23,3 +27,4 @@ for($i=0; $i<count($arrpc); $i++){
 echo "</td>";
 echo "</tr>";
 echo "</table>";
+
--- Template/SambaStatus/LastLogin.php
+++ Template/SambaStatus/LastLogin.php
@@ -1,23 +1,45 @@
 <?php
 //echo $view->header()->setAttribute('template', $T('LockedFiles'));
 
-$command = "sudo net ads search '(objectCategory=User)' name lastLogon | grep \":\" | tr -d \"\n\" | sed -e 's/name\: /,/g' | sed -e 's/lastLogon\: /|/g' | tr -s \",\" \"\n\" | sort";
+//$command = "sudo net ads search '(objectCategory=User)' name lastLogon displayName | grep \":\" | tr -d \"\n\" | sed -e 's/displayName\: /,/g' | sed -e 's/lastLogon\: /|/g' | sed -e 's/name\: /|/g' | tr -s \",\" \"\n\" | sort";
+//$command = "sudo net ads search '(objectCategory=User)' name lastLogon displayName | grep \":\" | tr -d \"\n\" | sed -e 's/displayName\: /,/g' | sed -e 's/lastLogon\: /|/g' | sed -e 's/name\: /|/g' | tr -s \",\" \"\n\" | awk -F \"|\" '{print $2 \"|\" $1 \"|\" $3}' | sort";
+$command = "sudo net ads search '(objectCategory=User)' name lastLogon displayName";
 $user = shell_exec($command);
-$arruser = explode("\n", $user);
-echo count($arruser) . " Total users - ";
+$user_explose = explode("\n\n", $user);
+unset($user_explose[0]);
+echo count($user_explose) . " Total users - ";
 $command = "sudo net ads search '(&(objectCategory=User)(lastLogon=0))' name | grep : | wc -l";
-echo shell_exec($command) . " Never logged in";
-echo "<hr />";
-echo "<table><tr><td><b>Username</b></td><td><b>Last Login Date</b></td></tr>";
-unset($arruser[0]);
-foreach($arruser as $arr){
-	$tmp = explode("|", $arr);
-	echo "<tr><td>". $tmp[0] . "</td><td>";	
-	if($tmp[1] == 0){
-		echo "<i>Never logged in</i>";
-	}else{
-		echo date("r" , ($tmp[1]/10000000)-11644473600);
+echo shell_exec($command) . " Never logged in<hr />";
+//echo "<table><tr><td><b>Username</b></td><td><b>Display Name</b></td><td><b>Last Login Date</b></td></tr>";
+
+$megatmp="";
+foreach($user_explose as $explose){
+	$foo = explode("\n", $explose);
+	$tmp = array();
+	foreach($foo as $boh){
+		$stecaz = explode(":", $boh);
+		$tmp[$stecaz[0]] = $stecaz[1];	
 	}
-	echo "</td></tr>";
+	$megatmp .= $tmp[name] . "|" . $tmp[displayName] . "|";
+	if($tmp[lastLogon] == 0){
+                $megatmp .= "<i>Never logged in</i>";
+        }else{
+                $megatmp .= date("r" , ($tmp[lastLogon]/10000000)-11644473600);
+        }
+	$megatmp .= "\n";
+	unset($tmp);
 }
+//echo nl2br($megatmp);
+echo "<table><tr><td><b>Username</b></td><td><b>Display Name</b></td><td><b>Last Login Date</b></td></tr>";
+//$megatmpsort=asort($megatmp);
+$megaarr=explode("\n", $megatmp);
+asort($megaarr);
+//print_r($megaarr);
+
+foreach($megaarr as $arr){
+        echo "<tr><td>";
+        echo str_ireplace("|", "</td><td>", $arr);
+        echo "</td></tr>\n";
+} 
 echo "</table>";
+//print_r($megaarr); 
